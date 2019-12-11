b282d6d31497cba0a50296036e637315729d291e
许仕昊 <18202952902@139.com>
2019-12-11 18:00:23 +0800

    融合security

--- src/main/java/com/tianxing/TianxingApplication.java
+++ src/main/java/com/tianxing/TianxingApplication.java
@@ -1,10 +1,23 @@
 package com.tianxing;
 
+import com.tianxing.config.MyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableConfigurationProperties({MyProperties.class})
+@EnableTransactionManagement
 @SpringBootApplication
 public class TianxingApplication {
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TianxingApplication.class, args);
